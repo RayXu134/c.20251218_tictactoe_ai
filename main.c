96ff055141d7a7f6e2023e9df261eb6da66cc1bc
pay <ka79411.yt@gmail.com>
2025-12-18 20:46:13 +0800

    Add tictactoe game and item structs

--- main.c
+++ main.c
@@ -3,6 +3,21 @@
 
 #include <ncurses.h>
 
+// @brief This has items in the tictactoe game board.
+enum TictactoeItem {
+  kO=0,
+  kX,
+  kEmpty
+};
+
+// @brief Store everything in the tictactoe game.
+struct Tictactoe {
+  // Board size.
+  int size;
+  // A 2d array stores items in the game.
+  enum TictactoeItem **board;
+};
+
 int main() {
   // ncurses Setup.
   initscr();
