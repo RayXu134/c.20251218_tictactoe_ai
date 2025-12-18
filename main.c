bbd1f9f8cb2c0462e7a5b7b7a7e2d2d56bcd3c4a
pay <ka79411.yt@gmail.com>
2025-12-18 20:36:51 +0800

    Add ncurses mode setup and exit

--- main.c
+++ main.c
@@ -1,2 +1,15 @@
 // tictactoe/main.c
 // Copyright (c) 2025 Ray. All Rights Reserved.
+
+#include <ncurses.h>
+
+int main() {
+  // ncurses Setup.
+  initscr();
+  cbreak();
+  noecho();
+  keypad(stdscr, TRUE);
+
+  // Exit ncurses mode.
+  endwin();
+}
