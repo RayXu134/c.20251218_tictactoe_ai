7306225ec07162ae2f7ba1bedadededaf4aff10b
pay <ka79411.yt@gmail.com>
2025-12-19 19:17:53 +0800

    Add init_tictactoe function

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 // Copyright (c) 2025 Ray. All Rights Reserved.
 
 #include <ncurses.h>
+#include <malloc.h>
 
 // @brief This has items in the tictactoe game board.
 enum TictactoeItem {
@@ -22,6 +23,10 @@ struct Tictactoe {
 // Function declarations
 // ---------------------
 
+// @brief Init a Tictactoe struct.
+// @param size The size of the tictactoe game.
+// @return 0 when success. -1 when error.
+int init_tictactoe(struct Tictactoe *pGame, const int size);
 
 // -------------
 // Main function
@@ -41,3 +46,30 @@ int main() {
 // ------------------------
 // Function implementations
 // ------------------------
+
+int init_tictactoe(struct Tictactoe *pGame, const int size) {
+  if (pGame == NULL) {
+    // Game pointer is NULL, we can't use it.
+    return -1;
+  }
+
+  pGame->size = size;
+
+  pGame->board = malloc(sizeof(struct Tictactoe *) * size);
+  if (pGame->board == NULL) {
+    // Malloc failed.
+    return -1;
+  }
+  for (int i = 0; i < size; i++) {
+    pGame->board[i] = malloc(sizeof(struct Tictactoe) * size);
+    if (pGame->board[i] == NULL) {
+      // Malloc failed.
+      return -1;
+    }
+    for (int j = 0; j < size; j++) {
+      pGame->board[i][j] = kEmpty;
+    }
+  }
+
+  return 0;
+}
