cda7045e1c4dad2c21679cb2265e69715a3ea4a0
pay <ka79411.yt@gmail.com>
2025-12-21 08:34:47 +0800

    Add is_moves_left function
    is_moves_left function can check if there's any empty
    cell on the board.

--- minimax.c
+++ minimax.c
@@ -1,4 +1,26 @@
 // tictactoe/minimax.c
 // Copyright (c) 2025 Ray. All Rights Reserved.
 
+#include <stdbool.h>
+#include <stddef.h>
+
 #include "minimax.h"
+
+// You can only use this static function in this file.
+// @brief Checks if there are any moves left on the board.
+// @return True if there are moves left, false otherwise.
+static int is_moves_left(struct Tictactoe *pGame) {
+  if (pGame == NULL) {
+    return false;
+  }
+  for (int i = 0; i < pGame->size; i++) {
+    for (int j = 0; j < pGame->size; j++) {
+      if (pGame->board[i][j] == kItemEmpty) {
+        return true;
+      }
+    }
+  }
+  // If the loops complete without finding an empty cell, it means the
+  // board is full.
+  return false;
+}
